8d848cca00ef3e2766b71c74d935d858ef155260
mac_ilong <wenlong.li@vhall.com>
2018-02-26 17:26:05 +0800

    add timer class

---
+++ AsyncTaskPool/VHTimer.cpp
@@ -0,0 +1,71 @@
+//
+//  VHTimer.cpp
+//  VhallSignaling
+//
+//  Created by ilong on 2017/12/19.
+//  Copyright © 2017年 ilong. All rights reserved.
+//
+
+#include "VHTimer.h"
+#include<thread>
+#include<chrono>
+#include<memory>
+
+VHTimer::VHTimer():
+mExpired(true){
+   
+}
+
+VHTimer::~VHTimer(){
+   Expire();
+}
+
+void VHTimer::StartTimer(int interval, std::function<void()> task){
+   if (mExpired == false){
+      return;
+   }
+   mExpired = false;
+   std::thread([this, interval, task](){
+      while (!mExpired){
+         std::unique_lock<std::mutex> lck(mMutex);
+         std::cv_status ret = mExpiredCond.wait_for(lck, std::chrono::milliseconds(interval));
+         if (ret == std::cv_status::timeout) {
+            task();
+         }
+      }
+   }).detach();
+}
+
+void VHTimer::Expire(){
+   if (mExpired) {
+      return;
+   }
+   mExpired = true;
+   mExpiredCond.notify_one();
+}
+
+template<typename callable, class... arguments>
+void VHTimer::SyncWait(int after, callable&& f, arguments&&... args){
+   mExpired = false;
+   std::function<typename std::result_of<callable(arguments...)>::type()> task
+   (std::bind(std::forward<callable>(f), std::forward<arguments>(args)...));
+   std::unique_lock<std::mutex> lck(mMutex);
+   std::cv_status ret = mExpiredCond.wait_for(lck, std::chrono::milliseconds(after));
+   if (ret == std::cv_status::timeout) {
+      task();
+   }
+}
+
+template<typename callable, class... arguments>
+void VHTimer::AsyncWait(int after, callable&& f, arguments&&... args){
+   mExpired = false;
+   std::function<typename std::result_of<callable(arguments...)>::type()> task
+   (std::bind(std::forward<callable>(f), std::forward<arguments>(args)...));
+   std::thread([this,after, task](){
+      std::unique_lock<std::mutex> lck(mMutex);
+      std::cv_status ret = mExpiredCond.wait_for(lck, std::chrono::milliseconds(after));
+      if (ret == std::cv_status::timeout) {
+         task();
+      }
+   }).detach();
+}
